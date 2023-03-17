42f52d7c820d1ed1c9471fc2b2e866829a9e1bb0
lukolik <110198144+lukolik@users.noreply.github.com>
2023-03-17 20:56:22 +0300

    Add files via upload

---
+++ vs-code-project-main/main.py
@@ -0,0 +1,15 @@
+import pz
+import pz2
+
+#pz.pz1()
+
+#pz.pz2()
+
+# pz.pz3()
+
+# pz.pz4()
+
+# pz.pz6() 
+
+
+pz2.pz3()
---
+++ vs-code-project-main/pz2.py
@@ -0,0 +1,41 @@
+def pz2():
+    file = open("pz1out.txt", "r", encoding='utf-8')
+    name = file.read()
+    slovar = {'а':'a','б':'b','в':'v','г':'g','д':'d','е':'e','ё':'yo',
+      'ж':'zh','з':'z','и':'i','й':'i','к':'k','л':'l','м':'m','н':'n',
+      'о':'o','п':'p','р':'r','с':'s','т':'t','у':'u','ф':'f','х':'h',
+      'ц':'c','ч':'ch','ш':'sh','щ':'sch','ъ':'','ы':'y','ь':'','э':'e',
+      'ю':'u','я':'ya', 'А':'A','Б':'B','В':'V','Г':'G','Д':'D','Е':'E','Ё':'YO',
+      'Ж':'ZH','З':'Z','И':'I','Й':'I','К':'K','Л':'L','М':'M','Н':'N',
+      'О':'O','П':'P','Р':'R','С':'S','Т':'T','У':'U','Ф':'F','Х':'H',
+      'Ц':'C','Ч':'CH','Ш':'SH','Щ':'SCH','Ъ':'','Ы':'y','Ь':'','Э':'E',
+      'Ю':'U','Я':'YA',',':'','?':'',' ':' ','~':'','!':'','@':'','#':'',
+      '$':'','%':'','^':'','&':'','*':'','(':'',')':'','-':'','=':'','+':'',
+      ':':'',';':'','<':'','>':'','\'':'','"':'','\\':'','/':'','№':'',
+      '[':'',']':'','{':'','}':'','ґ':'','ї':'', 'є':'','Ґ':'g','Ї':'i',
+      'Є':'e', '—':''}
+        
+   
+    for key in slovar:
+       name = name.replace(key, slovar[key])
+    print(name)
+
+
+def pz3():
+    listOut = []
+    while True:
+        fileName = input("enter name file")
+        if fileName == "quit":
+            break
+        else:
+            try:
+                file = open(fileName, "r", encoding="utf-8")
+                strFile = file.read()
+                listOut.append(strFile)
+            except:
+                print("file not found")
+            finally:
+                fileOut = open("result.txt", "w", encoding="utf-8")
+                for row in listOut:
+                    fileOut.write(row)
+
